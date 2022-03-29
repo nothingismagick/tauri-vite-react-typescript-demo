2881e9a9db080277617d26ee93cdd09a5ecd5600
Jonas Kruckenberg <iterpre@protonmail.com>
2022-03-29 22:53:06 +0200

    chore: set correct vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  // prevent vite from obscuring rust errors
+  clearScreen: false,
+  // tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
+  // to make use of `TAURI_PLATFORM`, `TAURI_ARCH`, `TAURI_FAMILY`, `TAURI_PLATFORM_VERSION`, `TAURI_PLATFORM_TYPE` and `TAURI_DEBUG` env variables
+  envPrefix: ["VITE_", "TAURI_"],
+  build: {
+    // tauri supports es2021
+    target: ["es2021", "chrome97", "safari13"],
+    // don't minify for debug builds
+    minify: process.env.TAURI_DEBUG ? false : "esbuild",
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_DEBUG,
+  },
+  plugins: [react()],
+});
