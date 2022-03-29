19df18338ba6aaf638826d1ca38cf4fd00c7dd3a
Jonas Kruckenberg <iterpre@protonmail.com>
2022-03-29 22:52:54 +0200

    feat: add overlay view

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,34 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
+use tauri::{window::WindowBuilder, AppHandle, WindowUrl};
+
+/// Create the confetti overlay window
+#[tauri::command]
+fn open_overlay_window(app: AppHandle) -> Result<(), String> {
+    let result = WindowBuilder::new(&app, "second", WindowUrl::App("/overlay".into()))
+        .decorations(false)
+        .transparent(true)
+        .maximized(true)
+        .build();
+
+    match result {
+        Ok(_) => {
+            println!("Window created successfully!");
+            Ok(())
+        }
+        Err(err) => {
+            println!("Failed to create Window {}", err);
+            Err("Failed to create Window".to_string())
+        }
+    }
+}
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![open_overlay_window])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import Overlay from './Overlay'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {location.pathname === '/overlay' && <Overlay />}
+    {location.pathname === '/' && <App />}
   </React.StrictMode>,
   document.getElementById('root')
 )
